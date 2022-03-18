257e114d89ad1bdc169d1911bfb6af7b017558be
Emily Hoodenpyle <emilyghoodenpyle@gmail.com>
2022-03-17 23:57:14 -0700

    modified js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//  TIMER
 var timerEl = document.getElementById('countdown');
 
 function countdown() {
@@ -15,3 +16,42 @@ function countdown() {
 }
 
 console.log(countdown())
+
+// HIDDEN ELEMENTS
+var container = document.querySelector(".container")
+
+container.addEventListener("click", function(event) {
+    var state = event.target;
+
+    if (element.matches(".box")) {
+        var state = element.getAttribute(data-state);
+
+        if (state === hidden) {
+            element.
+        }
+    }
+
+});
+
+// SUBMIT HIGHSCORE
+var submitEl = document.querySelector('#submit');
+var initialsInput = document.querySelector('#initials')
+function showResponse(event) {
+    event.preventDefault();
+    console.log(event);
+    var response = initialsInput.value + timeLeft;
+}
+
+// HIGHSCORE PAGE
+var body =  document.body;
+var highscoreEl = document.createElement("div");
+var listEl = document.createElement("ol");
+var li1 = document.createElement("li")
+var li2 = document.createElement("li")
+var li3 = document.createElement("li")
+
+li1.textContent = initialsInput.value;
+
+body.appendChild(highscoreEl)
+highscoreEl.appendChild(listEl);
+listEl.appendChild(li1)
